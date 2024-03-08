84d3ab9622f43ff5106d325d47615a5bb8613ae6
PHATCHARAPHON63 <phatcharaphonch63@nu.ac.th>
2024-03-08 13:37:54 +0700

    2

--- numeral/roman.py
+++ numeral/roman.py
@@ -1,2 +1,28 @@
-def to_roman(): 
-    """convert hindo Arabic number to Roman number.""" 
+def to_roman(arabic_num):
+    if not isinstance(arabic_num, int):
+        raise TypeError("arabic_num must be an integer")
+    if arabic_num <= 0 or arabic_num > 888:
+        raise ValueError("NumberOutOfRange")
+    roman_numerals = [
+    ("M", 1000),
+    ("CM", 900),
+    ("D", 500),
+    ("CD", 400),
+    ("C", 100),
+    ("XC", 90),
+    ("L", 50),
+    ("XL", 40),
+    ("X", 10),
+    ("IX", 9),
+    ("V", 5),
+    ("IV", 4),
+    ("I", 1),
+  ]
+
+    roman_num = ""
+    for symbol, value in roman_numerals:
+     while arabic_num >= value:
+       roman_num += symbol
+       arabic_num -= value
+
+    return roman_num
--- tests/numeral/test_roman.py
+++ tests/numeral/test_roman.py
@@ -1,6 +1,36 @@
-
+import pytest
 from numeral.roman import to_roman
 
+@pytest.mark.parametrize(
+  "arabic_num, roman_num",
+  [
+    (1, "I"),
+    (2, "II"),
+    (3, "III"),
+    (4, "IV"),
+    (5, "V"),
+    (10, "X"),
+    (888, "DCCCLXXXVIII"),
+    (42, "XLII"),
+    (99, "XCIX"),
+    (499, "CDXCIX"),
+    (888, "DCCCLXXXVIII"),
+  ]
+)
+def test_to_roman_success(arabic_num, roman_num):
+  assert to_roman(arabic_num) == roman_num
 
-def test_calling():
-    "A"
+@pytest.mark.parametrize(
+  "arabic_num",
+  [
+    0,
+    -1,
+    889,
+    1.5,
+    "abc",
+  ]
+)
+
+def test_to_roman_failure(arabic_num):
+    with pytest.raises(ValueError):
+      to_roman(arabic_num)
